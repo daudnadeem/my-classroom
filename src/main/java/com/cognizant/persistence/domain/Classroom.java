68c44c4830f3bc302ca42dac0e75cfe63637d1f2
Daud Nadeem <d.nadeem93@gmail.com>
2018-11-02 10:52:32 +0000

    added classroom.java

--- src/main/java/com/cognizant/persistence/domain/Classroom.java
+++ src/main/java/com/cognizant/persistence/domain/Classroom.java
@@ -1,5 +1,54 @@
 package com.cognizant.persistence.domain;
 
+import java.util.HashMap;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Classroom {
 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private long classroomID;
+	private String trainer;
+	@OneToMany
+	private HashMap<Integer,String> trainees = new HashMap<Integer,String>();  
+	
+	public Classroom() {
+		
+	}
+	
+	public Classroom(long classroomID, String trainer, HashMap<Integer,String> trainees) {
+		this.classroomID = classroomID;
+		this.trainer = trainer;
+		this.trainees = trainees;
+	}
+
+	public long getClassroomID() {
+		return classroomID;
+	}
+
+	public void setClassroomID(long classroomID) {
+		this.classroomID = classroomID;
+	}
+
+	public String getTrainer() {
+		return trainer;
+	}
+
+	public void setTrainer(String trainer) {
+		this.trainer = trainer;
+	}
+
+	public HashMap<Integer, String> getTrainees() {
+		return trainees;
+	}
+
+	public void setTrainees(HashMap<Integer, String> trainees) {
+		this.trainees = trainees;
+	}
 }
